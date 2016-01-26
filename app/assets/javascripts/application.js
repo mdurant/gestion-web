47a078b94428c914a6ea7531fefed9e9235b9ed3
Mauricio Duran <mauricio@MBP-de-Mauricio.iptv.microsoft.com>
2016-01-26 01:16:22 -0300

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,26 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+
+//= require flow-chart/bar-chart.js
+//= require flow-chart/curved-line-chart.js
+//= require flow-chart/dynamic-chart.js
+//= require flow-chart/line-chart.js
+//= require flow-chart/pie-chart.js
+//= require charts.js
+//= require demo.js
+//= require functions.js
+//= require_tree .
+
